6ebe2019734d5736e0c40a6f3478933d0cf6126e
Joseph Werle <joseph.werle@gmail.com>
2019-05-05 16:23:39 -0700

    refactor(example.js): Add 'valueEncoding' example

--- example.js
+++ example.js
@@ -9,3 +9,16 @@ const hello = codec.encode('hello')
 const world = codec.encode('world')
 
 console.log('%s %s', codec.decode(hello), codec.decode(world)) // 'hello world'
+
+const pbs = require('protocol-buffers')
+
+const { Message } = pbs(`
+message Message {
+  bytes data = 1;
+}
+`)
+
+const codecx = Codec(nonce, key, { valueEncoding: Message })
+const codecy = Codec(nonce, key, { valueEncoding: Message })
+const buffer = codecx.encode({ data: Buffer.from('hello world') })
+console.log(codecy.decode(buffer))
